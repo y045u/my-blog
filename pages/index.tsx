ad33bbd88f4036c24bd707c0a47b6f0485c920fa
y045u <fukasawa0405.tech@gmail.com>
2025-03-17 03:09:10 +0900

    🚀 新規で再作成

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className=""></main>
+    </div>
+  );
+}
