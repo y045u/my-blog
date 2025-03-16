24a7df8b2558b46dff34da9142da53e2f2fe81e7
y045u <fukasawa0405.tech@gmail.com>
2025-03-17 05:54:47 +0900

    💄 update design

--- components/Post/SinglePost.tsx
+++ components/Post/SinglePost.tsx
@@ -17,7 +17,7 @@ const SinglePost = (props: Props) => {
   return (
     <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
       <div className="flex items-center gap-3">
-        <h2 className="text-gray-100">
+        <h2 className="text-gray-100 text-2xl font-medium">
           <Link href={`/posts/${slug}`}>{title}</Link>
         </h2>
         <div className="text-gray-100">{create_date.substring(0, 10)}</div>
