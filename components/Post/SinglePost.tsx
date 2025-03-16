df27cc39c142808a822563813ab03d01209e1dc1
y045u <fukasawa0405.tech@gmail.com>
2025-03-17 05:52:19 +0900

    🐛 ハイドレーション回避

--- components/Post/SinglePost.tsx
+++ components/Post/SinglePost.tsx
@@ -15,18 +15,18 @@ const SinglePost = (props: Props) => {
   const { id, title, description, create_date, update_date, slug, tags } = props;
 
   return (
-    <Link href={`/posts/${slug}`}>
-      <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
-        <div className="flex items-center gap-3">
-          <h2 className="text-gray-100">{title}</h2>
-          <div className="text-gray-100">{create_date.substring(0, 10)}</div>
-          {tags.map((tag) => (
-            <span className="text-white bg-gray-500 rounded-xl px-2 pb-1 font-medium">{tag}</span>
-          ))}
-        </div>
-        <p className="text-gray-100">{description}</p>
-      </section>
-    </Link>
+    <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
+      <div className="flex items-center gap-3">
+        <h2 className="text-gray-100">
+          <Link href={`/posts/${slug}`}>{title}</Link>
+        </h2>
+        <div className="text-gray-100">{create_date.substring(0, 10)}</div>
+        {tags.map((tag) => (
+          <span className="text-white bg-gray-500 rounded-xl px-2 pb-1 font-medium">{tag}</span>
+        ))}
+      </div>
+      <p className="text-gray-100">{description}</p>
+    </section>
   );
 };
 
